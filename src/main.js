a405e4167c321e99527d6bab02604b649f901b94
unknown <sx-9548@dtdream.com>
2019-02-15 14:24:43 +0800

    美化

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import router from './router'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import NormailizeCss from 'normalize.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
