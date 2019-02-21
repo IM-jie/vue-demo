a27a3ce80468c11509f745cf7eab39a969400365
jie <jie@jiedeMacBook-Pro.local>
2019-02-21 22:09:43 +0800

    修改g6

--- src/pages/BasicLayout/BasicLayout.vue
+++ src/pages/BasicLayout/BasicLayout.vue
@@ -3,38 +3,50 @@
 </template>
 
 <script>
-  import G6 from '@antv/g6'
-
-  const data = {
-    nodes: [{
-      id: 'node1',
-      x: 100,
-      y: 200
-    },{
-      id: 'node2',
-      x: 300,
-      y: 200
-    }],
-    edges: [{
-      id: 'edge1',
-      target: 'node2',
-      source: 'node1'
-    }]
-  };
+  import G6 from '@antv/g6';
+  import '@antv/g6/build/plugin.tool.grid';
 
   export default {
     name: 'BasicLayout',
     mounted () {
       this.initG6()
-    }, methods: {
+    },
+    data () {
+      return {
+        grid:'',
+        graph:'',
+
+        data: {
+          nodes: [{
+            id: 'node1',
+            x: 100,
+            y: 200
+          }, {
+            id: 'node2',
+            x: 300,
+            y: 200
+          }],
+          edges:
+            [{
+              id: 'edge1',
+              target: 'node2',
+              source: 'node1'
+            }]
+        }
+      }
+    },
+    methods: {
       initG6 () {
-        const graph = new G6.Graph({
+        this.grid = new G6.Plugins['tool.grid']();
+
+        this.graph = new G6.Graph({
           container: 'mountNode',
           width: 500,
-          height: 500
-        });
-        graph.read(data);
-        return graph;
+          height: 500,
+          plugins: [this.grid]
+        })
+        this.graph.read(this.data)
+        return graph
       }
     }
   }
